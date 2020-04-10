c4edd22e7769d3817bdd62f89a4036661de83d9b
oscar <oscar@valordistributions.com>
2020-04-10 11:45:13 -0400

    complex logic to generate product combinations

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 class ProductRepository extends RepositoryService
 {
 
+    private $result = [];
     private $generate = false; // GENERATE PRODUCT FAMILY
 
     public function getList(array $searchCriteria = [])
@@ -91,14 +92,56 @@ class ProductRepository extends RepositoryService
 
             if ($this->generate == true) // IT CAMES FROM PRODUCT FAMILY
             {
-                $family_id = $this->createFamily($data);
+                $family_id = $this->createFamily($data); // FIRST WE CREATE THE FAMILY
             }
 
             $data["family_id"] = $family_id;
-            parent::store($data);
-            $this->createAttribute($data, $family_id); // SAVE PRODUCT ATTRIBUTES
+
+            if ($this->generate == true) {
+                $this->generateFamily($data, $family_id); // GENERATE FAMILY
+            } else {
+                parent::store($data);
+                $this->createAttribute($data); // CREATE ATTRIBUTE
+            }
+
         });
 
+    }
+
+    private function createAttribute($data) {
+
+        if (isset($data["prod_attributes"]))
+        {
+            $product_id         = $this->model->id; //SAVE CURRENT PRODUCT ID
+            $object             = $data["prod_attributes"];
+            $row                = 0;
+            $tot                = 0;
+            $sku_increment      = 1;
+            array_push($all_products_id, $product_id);
+
+            $row=0;
+
+            foreach ($object as $attributes) // EACH ATTRIBUTE
+            {
+                $row++;
+                $tot = count($attributes); // TOTAL ATTRIBUTES
+
+                if ($row==1) // ONE ROW CONTAINS ALL ATTRIBUTES
+                {
+                    for ($i=0; $i < $tot; $i++) // WHILE FOUND ATTRIBUTES
+                    {
+                        if (isset($attributes[$i]))
+                        {
+                            if ($attributes[$i]!=0)
+                            {
+                                $get_array = $attributes[$i];
+                                $this->saveProductAttribute($product_id, $get_array["id"], $get_array["value"], $family_id);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private function createFamily($data)
@@ -121,7 +164,7 @@ class ProductRepository extends RepositoryService
     public function update($model, array $data)
     {
         parent::update($model, $data);
-        $this->createAttribute($data, isset($data["family_id"]) ? $data["family_id"] : null); // SAVE PRODUCT ATTRIBUTES
+        $this->generateFamily($data, isset($data["family_id"]) ? $data["family_id"] : null); // SAVE PRODUCT ATTRIBUTES
     }
 
     private function saveProductAttribute($product_id, $attribute_id, $value, $family_id)
@@ -134,21 +177,14 @@ class ProductRepository extends RepositoryService
         [
             'value' => $value
         ]);
-
-        if ($this->generate == true)
-        {
-            // SAVE ATTRIBUTES FOR THE FAMILY
-            ProductFamilyAttribute::updateOrCreate(['family_id' => $family_id, 'attribute_id' => $attribute_id, 'value' => $value]);
-        }
     }
 
-    public function createAttribute($data, $family_id = null)
+    public function generateFamily($data, $family_id = null)
     {
         if (isset($data["prod_attributes"]))
         {
-            $product_id         = $this->model->id; //SAVE CURRENT PRODUCT ID
+
             $all_products_id    = [];
-            $ids                = $product_id;
             $object             = $data["prod_attributes"];
             $attrib             = array(array());
             $row                = 0;
@@ -156,10 +192,7 @@ class ProductRepository extends RepositoryService
             $sku_family         = $data["sku"];
             $product_name       = $data["name"];
             $sku_increment      = 1;
-            array_push($all_products_id, $product_id);
-
-            // REMOVE ALL TO INSERT AGAIN
-            // ProductAttribute::where('product_id', $product_id)->delete();
+            $attributes_concat  = [];
 
             $row=0;
 
@@ -177,38 +210,9 @@ class ProductRepository extends RepositoryService
                             if ($attributes[$i]!=0)
                             {
                                 $get_array = $attributes[$i];
-
-                                // FIND IF PRODUCT ALREADY HAS THE ATTRIBUTE
-                                $find = ProductAttribute::where(['product_id' => $product_id, 'attribute_id' => $get_array["id"]])->select('value')->first();
-
-                                if ($find) { //FOUND THIS ATTRIBUTE FOR THE PRODUCT
-                                    if ($find->value != $get_array["value"]) {  // SAME ATTRIBUTE, BUT DIFFERENT VALUE. CREATE A NEW PRODUCT
-
-                                        // CREATE PRODUCT FAMILY
-                                        if ($this->generate == true)
-                                        {
-                                            $data["sku"] = $sku_family . " - " . $sku_increment;  // NEW SKU
-                                            $sku_increment++; // INCREMENT FOR THE NEXT PRODUCT FAMILY
-                                            $data["family_id"] = $family_id; // SET FAMILY ID FOR THIS PRODUCT
-
-                                            parent::store($data); // CREATE A NEW PRODUCT
-                                            $new_product_id = $this->model->id; //GETTING PRODUCT ID CREATED
-
-                                            array_push($all_products_id, $new_product_id); // SAVE ALL PRODUCTS CREATED
-                                            $ids .= ", " . $new_product_id; //SAVING BY COMMA ALL PRODUCT IDS
-
-                                            $this->saveProductAttribute($new_product_id, $get_array["id"], $get_array["value"], $family_id); // CREATE NEW PRODUCT ATTRIBUTE
-                                        }
-                                        else
-                                        {
-                                            $this->saveProductAttribute($product_id, $get_array["id"], $get_array["value"], $family_id);
-                                        }
-
-                                    }
-                                } else { // CREATING PRODUCT ATTRIBUTE
-                                    $this->saveProductAttribute($product_id, $get_array["id"], $get_array["value"], $family_id);
-                                }
-
+                                // SAVING ALL FAMILY ATTRIBUTES
+                                ProductFamilyAttribute::updateOrCreate(['family_id' => $family_id, 'attribute_id' => $get_array["id"], 'value' => $get_array["value"]]);
+                                $attributes_concat[$i] = $get_array["id"] . "|" . $get_array["value"];
                             }
                         }
 
@@ -216,102 +220,147 @@ class ProductRepository extends RepositoryService
                 }
             }
 
-            // CREATE PRODUCT FAMILY
-            if ($this->generate == true)
+
+            $tot_attributes = DB::table('product_family_attributes')->distinct('attribute_id')->count('attribute_id');
+            $r              = $tot_attributes;
+            $n              = sizeof($attributes_concat);
+            $this->printCombination($attributes_concat, $n, $r);
+            $my_array       = [];
+            $my_array       = $this->result;
+
+            /* EXAMPLE
+            Array
+            (
+                [0] => 3|sour,2|50ml,1|50mg,
+                [1] => 3|sour,2|50ml,1|20mg,
+                [2] => 3|sour,2|50ml,1|35mg,
+                [3] => 3|sour,2|30ml,1|50mg,
+                [4] => 3|sour,2|30ml,1|20mg,
+                [5] => 3|sour,2|30ml,1|35mg,
+                [6] => 3|salt,2|50ml,1|50mg,
+                [7] => 3|salt,2|50ml,1|20mg,
+                [8] => 3|salt,2|50ml,1|35mg,
+                [9] => 3|salt,2|30ml,1|50mg,
+                [10] => 3|salt,2|30ml,1|20mg,
+                [11] => 3|salt,2|30ml,1|35mg,
+                )
+            */
+
+            // HERE WE NEED TO LOOP COMBINATIONS AND CREATE ONE PRODUCT BY ROW
+            foreach ($my_array as $index => $element)
             {
-                // FIND MAIN PRODUCT (WITH MORE ATTRIBUTES SAVED)
-                $sQuery = 'SELECT product_id, count(attribute_id) as tot FROM product_attributes WHERE product_id IN (' . $ids .') GROUP BY product_id ORDER BY 2 DESC LIMIT 1';
-                echo $sQuery . " FIND MAIN PRODUCT (WITH MORE ATTRIBUTES SAVED) " . "\n";
-                $main_prod = DB::select($sQuery);
-                if ($main_prod)
+                // CONCAT ATTRIBUTES TO SAVE ON PRODUCT NAME
+                $concat_name = "";
+
+                // NEW SKU
+                $data["sku"] = $sku_family . " - " . $sku_increment;
+
+                // INCREMENT FOR THE NEXT PRODUCT FAMILY
+                $sku_increment++;
+
+                // SET FAMILY ID FOR THIS PRODUCT
+                $data["family_id"] = $family_id;
+
+                // CREATE A NEW PRODUCT
+                parent::store($data);
+
+                //GETTING PRODUCT ID CREATED
+                $new_product_id = $this->model->id;
+
+                // SAVE ALL PRODUCTS CREATED
+                array_push($all_products_id, $new_product_id);
+
+                // [0] => 3|sour,2|50ml,1|50mg - EXAMPLE
+                $attributes = explode(',', $element);
+
+                // LOOP ROW OF ATTRIBUTES
+                foreach ($attributes as $v)
                 {
-                    foreach ($all_products_id as $key => $value) // EACH ALL PRODUCTS CREATED
+                    if (isset($v[0]))
                     {
-                        if ($value != $main_prod[0]->product_id) // WE DON'T NEED THE MAIN PRODUCT
-                        {
-                            // GET THE ATTRIBUTES FROM THE MAIN PRODUCT - WE NEED TO CHECK WHICH ATTRIBUTE THE CHILD NEED INHERANCE FROM PARENT
-                            $child = $value;
-                            $sQuery = 'SELECT attribute_id, value FROM product_attributes WHERE product_id = ' . $main_prod[0]->product_id . ' and attribute_id NOT IN (SELECT attribute_id FROM product_attributes WHERE product_id = ' . $child . ')';
-                            echo " GET THE ATTRIBUTES FROM THE MAIN PRODUCT - WE NEED TO CHECK WHICH ATTRIBUTE THE CHILD NEED INHERANCE FROM PARENT " . $sQuery . "\n";
-                            $get_attributes = DB::select($sQuery);
-
-                            foreach ($get_attributes as $v) // EACH ATTRIBUTE FROM THE PARENT
-                            {
-                                // CREATE NEW ATTRIBUTE FOR CHILD
-                                $this->saveProductAttribute($child, $v->attribute_id, $v->value, $family_id);
-                            }
-                        }
-                    }
-                }
-
-
-                // GET PRODUCT WITH MORE ATTRIBUTES
-                $sQuery = 'SELECT product_id, count(attribute_id) as tot FROM product_attributes WHERE product_id IN (' . $ids .') GROUP BY product_id ORDER BY 2 DESC LIMIT 1';
-                $main_prod = DB::select($sQuery);
-                echo " MORE ATTRIBUTES " . $sQuery . "\n";
-                if ($main_prod)
-                {
-                    // GET THE ATTRIBUTES FROM THE MAIN PRODUCT
-                    $sQuery = 'SELECT attribute_id, value FROM product_attributes WHERE product_id in (' . $ids . ') and value not in (SELECT value FROM product_attributes WHERE product_id = ' . $main_prod[0]->product_id . ')';
-                    $get_attributes = DB::select($sQuery);
-                    echo "GET THE ATTRIBUTES FROM THE MAIN PRODUCT " . $sQuery . "\n";
-
-                    $data["sku"]  = $sku_family . " - " . $sku_increment;  // NEW SKU
-                    $sku_increment++; // INCREMENT FOR THE NEXT PRODUCT FAMILY
-                    $data["family_id"] = $family_id; // SET FAMILY ID FOR THIS PRODUCT
-
-                    parent::store($data); // CREATE A NEW PRODUCT
-                    $new_product_id = $this->model->id;
-
-                    array_push($all_products_id, $new_product_id); // SAVE ALL PRODUCTS CREATED
-                    $ids .= ", " . $new_product_id; //SAVING BY COMMA ALL PRODUCT IDS
-
-                    foreach ($get_attributes as $v)
-                    {
-                        // CREATE NEW ATTRIBUTE FOR CHILD
-                        $this->saveProductAttribute($new_product_id, $v->attribute_id, $v->value, $family_id);
-                    }
-
-                    // FINAL STEP - FIND MISSING ATTRIBUTES FOR THE NEW PRODUCT CREATED
-                    $sQuery = 'SELECT DISTINCT a1.attribute_id, a2.product_id, a1.value
-                    FROM product_attributes AS a1
-                    CROSS JOIN product_attributes as a2
-                    WHERE a1.product_id IN (' . $ids . ')  AND a1.attribute_id not in (SELECT attribute_id FROM product_attributes WHERE product_id = a2.product_id ) ';
-                    $get_attributes = DB::select($sQuery);
-
-                    echo " HERE " . $sQuery . "\n";
-
-                    // SAVE MIXED ATTRIBUTES FOR EACH PRODUCT
-                    foreach ($get_attributes as $v)
-                    {
-                        $this->saveProductAttribute($new_product_id, $v->attribute_id, $v->value, $family_id);
+                        $get_array = explode('|', $v); // 3|sour 2|50ml 1|50mg - ALL ATTRIBUTES FOR ONE PRODUCT
+                        $this->saveProductAttribute($new_product_id, $get_array[0], $get_array[1], $family_id); // CREATE NEW PRODUCT ATTRIBUTE
+                        $concat_name .= " - " . $get_array[1];
                     }
 
                 }
 
                 // CONCAT PRODUCT NAME WITH ATTRIBUTE VALUE
-                $read            = DB::select('SELECT product_id, attribute_id, value, p.name FROM product_attributes pa INNER JOIN products p ON p.id = pa.product_id WHERE product_id IN (' . $ids .') ORDER BY product_id, attribute_id');
-                $new_name        = "";
-                $product_control = "";
+                Product::where('id', $new_product_id)->update(['name' => $data["name"] . $concat_name]);
+            }
 
-                foreach ($read as $v)
-                {
-                    Product::where('id', $v->product_id)->update(['name' => ($new_name=="" ? ($v->name . " - " . $v->value) : ($new_name . " - " . $v->value))]);
+        }
+    }
 
-                    // RESET WHEN CHANGE PRODUCT
-                    if ($product_control!="" || $product_control != $v->product_id) {
-                        $new_name = "";
-                        $product_control = $v->product_id;
+    private function printCombination($arr, $n, $r)
+    {
+        // A temporary array to store all combination one by one
+        $data = Array();
+
+        // save all combination using temprary array 'data[]'
+        return $this->combinationUtil($arr, $n, $r, 0, $data, 0);
+    }
+
+    /*
+        arr[]   ---> Input Array
+        n       ---> Size of input array
+        r       ---> Size of a combination to be saved
+        index   ---> Current index in data[]
+        data[]  ---> Temporary array to store current combination
+        i       ---> index of current element in arr[]
+    */
+    private function combinationUtil($arr, $n, $r, $index, $data, $i)
+    {
+        $qt_elements    = 0;
+        $comb           = "";
+
+        // Current cobination is ready, save it
+        if ($index == $r)
+        {
+            for ($j = 0; $j < $r; $j++)
+            {
+                if (isset($data[$j-1])) {
+                    if (substr($data[$j],0,1) != substr($data[$j-1],0,1)) {
+                        $comb .= $data[$j] . ",";
+                        $qt_elements++;
                     }
-
-                    if ($new_name=="") { // CONCAT NAME + ATTRIBUTE VALUE
-                        $new_name = $v->name . " - " . $v->value;
-                    } else {
-                        $new_name = $new_name . " - " . $v->value;
-                    }
+                } else {
+                    $comb .= $data[$j] . ",";
+                    $qt_elements++;
                 }
             }
+
+            // WE NEED THE COMPLETE COMBINATION (# r passed as parameter)
+            if ($qt_elements == $r)
+            {
+                array_push($this->result, $comb);
+            }
+
+            return;
         }
+
+        // When no more elements are there to put in data[]
+        if ($i >= $n) {
+            return;
+        }
+
+        // current is included, put next at next location
+        $data[$index] = $arr[$i];
+        $this->combinationUtil($arr, $n, $r,
+                        $index + 1,
+                        $data, $i + 1);
+
+        // Remove duplicates
+        if (isset($arr[$i]) && isset($arr[$i+1])) {
+            while ($arr[$i] == $arr[$i+1])
+            {
+                $i++;
+            }
+        }
+
+        // current is excluded, replace it with next (Note that i+1 is passed, but index is not changed)
+        $this->combinationUtil($arr, $n, $r, $index, $data, $i + 1);
+
     }
 
 }
