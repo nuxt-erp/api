8c9f6c4e2ff627385fa7fe1a6340faa5c7529336
root <avani@valordistributions.com>
2020-09-10 17:47:47 -0400

    changes

--- Modules/Inventory/Transformers/ProductResource.php
+++ Modules/Inventory/Transformers/ProductResource.php
@@ -33,19 +33,11 @@ class ProductResource extends ResourceService
             'weight'                => $this->weight,
             'launch_at'             => $this->launch_at,
             'is_enabled'            => $this->is_enabled,
-<<<<<<< HEAD
-            'sales_channel'         => $this->sales_channel,
-            
-            'stock_locator'         => $this->stock_locator,
-            'stock_locator_name'    => optional($this->stock_locator)->name,
-            
-=======
             'sales_channel'         => $this->sales_channel,            
-            'stock_locator'         => $this->stock_locator_id,
-            'stock_locator_name'    => optional($this->stock_locator)->name,
+            'stock_locator'         => $this->stock_locator,
+            'stock_locator_name'    => optional($this->stock_locator_name),
             'measure_id'            => $this->measure_id,
             'measure_name'            => optional($this->measure)->name,
->>>>>>> origin
             'name_full'             => $this->full_description,
             'product_attributes'    => $this->details,
             'in_transit_suppliers'  => $this->getInTransitAttribute($this->id),
