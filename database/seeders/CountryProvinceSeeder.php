667093a3dc098edf792ac9f236d093aba9be996a
root <avani@valordistributions.com>
2020-09-21 12:20:44 -0400

    add seeder

---
+++ database/seeders/CountryProvinceSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Country;
+use App\Models\Province;
+
+class CountryProvinceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $canada = Country::firstOrCreate([
+            'name' => 'Canada'
+        ]);
+
+        Province::updateOrcreate([
+            'name'      => 'Ontario',
+            'code'      => 'ON',
+            'country_id'=> $canada->id
+        ]);
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        Role::updateOrCreate([
+            'code' => 'user',
+            'name' => 'User'
+        ]);
+
+        Role::updateOrCreate([
+            'code' => 'admin',
+            'name' => 'Admin'
+        ]);
+    }
+}
