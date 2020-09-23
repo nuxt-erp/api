d5bf998479187f9b07b53dba9ed13c2c9ad122e3
root <avani@valordistributions.com>
2020-09-23 17:23:26 -0400

    ad changes

---
+++ database/seeders/ModuleSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Module;
+use Illuminate\Database\Seeder;
+
+class ModuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Module::updateOrCreate([
+            'name'  => 'Inventory'
+        ]);
+
+        Module::updateOrCreate([
+            'name'  => 'Sales'
+        ]);
+
+        Module::updateOrCreate([
+            'name'  => 'RD'
+        ]);
+
+        Module::updateOrCreate([
+            'name'  => 'Production'
+        ]);
+
+        Module::updateOrCreate([
+            'name'  => 'ExpensesApproval'
+        ]);
+
+        Module::updateOrCreate([
+            'name'  => 'Purchase'
+        ]);
+    }
+}
