e57e653e73ed2aeffed437aeb29479a237c14156
Luiz Valente <luiz@valordistributions.com>
2020-10-02 10:35:42 -0400

    In Progress

---
+++ Modules/Inventory/Database/Migrations/schema/2020_10_02_140346_create_custom_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('tenant')->create('inv_prod_custom_prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('inv_products')->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->decimal('price', 10, 4)->default(0);
+            $table->boolean('is_enabled')->default(1);
+            $table->timestamp('disabled_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('tenant')->dropIfExists('inv_prod_custom_prices');
+    }
+}
