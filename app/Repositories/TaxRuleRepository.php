f5c181fead2ae7ce472b421d7c05815a3ebc73e7
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2020-10-28 10:36:07 -0400

    Create for tax rules finished

--- app/Repositories/TaxRuleRepository.php
+++ app/Repositories/TaxRuleRepository.php
@@ -2,8 +2,65 @@
 
 namespace App\Repositories;
 
+use App\Models\TaxRuleComponent;
+use App\Models\TaxRuleScope;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Arr;
 
 class TaxRuleRepository extends RepositoryService
 {
+    public function store(array $data)
+    {
+
+        DB::transaction(function () use ($data)
+        {
+            parent::store($data);
+            
+            if(!empty($data['tax_details'])) {
+                $tax_component = $data['tax_details'];
+                $new                    = new TaxRuleComponent();
+                $new->tax_rule_id       = $this->model->id;
+                $new->component_name    = $tax_component['component_name'];
+                $new->seq               = $tax_component['seq'];
+                $new->rate              = $tax_component['rate'];
+                $new->compound          = $tax_component['compound'];
+                $new->save();
+            }
+            if(!empty($data['scope_names'])) {
+                $scope_names = $data['scope_names'];
+                foreach ($scope_names as $scope_name) {
+                    $new                  = new TaxRuleScope();
+                    $new->tax_rule_id     = $this->model->id;
+                    $new->scope           = $scope_name;
+                    $new->save();
+                }
+            }
+        });
+    }
+    public function update($model, array $data)
+    {
+
+        DB::transaction(function () use ($data, $model)
+        {    
+            parent::update($model, $data);
+            if(!empty($data['tax_details'])) {
+                $tax_component = $data['tax_details'];
+                TaxRuleComponent::updateOrCreate(
+                    ['id' => $tax_component['id']],
+                    $tax_component
+                );
+            }
+            if(!empty($data['scope_names'])) {
+                TaxRuleScope::where('tax_rule_id', $model->id)->delete();
+                
+                $scope_names = $data['scope_names'];
+                foreach ($scope_names as $scope_name) {
+                    $new                  = new TaxRuleScope();
+                    $new->tax_rule_id     = $this->model->id;
+                    $new->scope           = $scope_name;
+                    $new->save();
+                }
+            }
+        });
+    }
 }
--- app/Resources/TaxRuleResource.php
+++ app/Resources/TaxRuleResource.php
@@ -7,6 +7,7 @@ class TaxRuleResource extends ResourceService
 {
     public function toArray($request)
     {
+        lad($this->scopes->pluck('scope')->toArray());
         return [
             'id'                    => $this->id,
             'name'                  => $this->name,
@@ -18,7 +19,7 @@ class TaxRuleResource extends ResourceService
             'province_id'           => $this->province_id,
             'province_name'         => optional($this->province)->name,
             'province_code'         => optional($this->province)->code,
-            'scopes'                => implode(', ', $this->scopes->pluck('scope')->toArray()),
+            'scope_names'     => optional($this->scopes)->pluck('scope')->toArray(),
             'created_at'            => optional($this->created_at)->format('Y-m-d H:i:s'),
             'updated_at'            => optional($this->updated_at)->format('Y-m-d H:i:s'),
         ];
