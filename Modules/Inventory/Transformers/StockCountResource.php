8a36e6d9ab06495ddcfa7699dc5f54bbae8e3f57
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2020-12-04 15:32:23 -0500

    Stock count resource changes

--- Modules/Inventory/Transformers/StockCountResource.php
+++ Modules/Inventory/Transformers/StockCountResource.php
@@ -8,6 +8,31 @@ class StockCountResource extends ResourceService
 {
     public function toArray($request)
     {
+        $filters = $this->stock_filters;
+        lad($filters);
+        $tag_ids = [];
+        $brand_ids = [];
+        $category_ids = [];
+        $stock_locator_ids = [];
+        $bin_ids = [];
+        foreach($filters as $filter) {
+            if($filter->type === 'App\\Models\\Tag') {
+                array_push($tag_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\StockLocator') {
+                array_push($stock_locator_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\LocationBin') {
+                array_push($bin_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\Category') {
+                array_push($category_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\Brand') {
+                array_push($brand_ids, $filter->type_id);
+            }
+        }
+
         return [
             'id'                        => $this->id,
             'name'                      => $this->name,
@@ -23,7 +48,11 @@ class StockCountResource extends ResourceService
             'category_name'             => optional($this->category)->name,
             'location_id'               => $this->location_id,
             'location_name'             => optional($this->location)->name,
-            'stock_filters'             => optional($this->stock_filters)->toArray(),
+            'tag_ids'                   => $tag_ids,
+            'stock_locator_ids'         => $stock_locator_ids,
+            'bin_ids'                   => $bin_ids,
+            'category_ids'              => $category_ids,
+            'brand_ids'                 => $brand_ids,
             'net_variance'              => $this->net_variance,
             'abs_variance'              => $this->abs_variance,
             'success_rate'              => $this->success_rate,
