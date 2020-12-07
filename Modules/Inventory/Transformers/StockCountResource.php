650b7ba1a59fb29bcdf3352b0a6e111eb2105bdb
Matthew Wierzbicki <mateuszwierzbicki97@gmail.com>
2020-12-07 09:34:33 -0500

    Stock COunt resource

--- Modules/Inventory/Transformers/StockCountResource.php
+++ Modules/Inventory/Transformers/StockCountResource.php
@@ -8,10 +8,35 @@ class StockCountResource extends ResourceService
 {
     public function toArray($request)
     {
+        $filters = $this->stock_filters;
+        $tag_ids = [];
+        $brand_ids = [];
+        $category_ids = [];
+        $stock_locator_ids = [];
+        $bin_ids = [];
+        
+        foreach($filters as $filter) {
+            if($filter->type === 'App\\Models\\Tag') {
+                array_push($tag_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\StockLocator') {
+                array_push($stock_locator_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\LocationBin') {
+                array_push($bin_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\Category') {
+                array_push($category_ids, $filter->type_id);
+            }
+            else if($filter->type === 'Modules\\Inventory\\Entities\\Brand') {
+                array_push($brand_ids, $filter->type_id);
+            }
+        }
+
         return [
             'id'                        => $this->id,
             'name'                      => $this->name,
-            'date'                      => $this->date->format('Y-m-d'),
+            'date'                      => optional($this->date)->format('Y-m-d'),
             'target'                    => $this->target,
             'count_type_id'             => $this->count_type_id,
             'is_enabled'                => $this->add_discontinued,
@@ -23,6 +48,11 @@ class StockCountResource extends ResourceService
             'category_name'             => optional($this->category)->name,
             'location_id'               => $this->location_id,
             'location_name'             => optional($this->location)->name,
+            'tag_ids'                   => $tag_ids,
+            'stock_locator_ids'         => $stock_locator_ids,
+            'bin_ids'                   => $bin_ids,
+            'category_ids'              => $category_ids,
+            'brand_ids'                 => $brand_ids,
             'net_variance'              => $this->net_variance,
             'abs_variance'              => $this->abs_variance,
             'success_rate'              => $this->success_rate,
